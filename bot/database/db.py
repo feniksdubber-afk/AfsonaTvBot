0f933fafc4378430d46d3402e691bab2e163f7a2
feniksdubber-afk <feniksdubber@gmail.com>
2026-05-24 14:52:28 +0500

    Update db.py

--- bot/database/db.py
+++ bot/database/db.py
@@ -1,10 +1,13 @@
+import os
 import aiosqlite
 from bot.config import DB_PATH
+from bot.database.models import _create_tables
 
 async def get_db():
     return await aiosqlite.connect(DB_PATH)
 
 async def init_db():
+    os.makedirs(os.path.dirname(DB_PATH), exist_ok=True)
     async with aiosqlite.connect(DB_PATH) as db:
         await _create_tables(db)
         await db.commit()
